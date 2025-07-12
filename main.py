b973e08ab698cbb090c9103c47359c0acf3bcf26
ppsjai <93466089+ppsjai@users.noreply.github.com>
2025-07-11 22:24:34 -0700

    done a little change main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 # Password Generator Project
-# how de we provide credentials in UI
+# how de we provide credentials in that UI
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle # instead of calling random.randint() we can use randint key alone
 import pyperclip
 
-#TODO --------------------------------------- PASSWORD GENERATOR ----------------------------------------------------------------------------- #
+#TODO ---------------------------- PASSWORD GENERATOR ---------------------------------------------------------- #
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-           'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
-           'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+', "-", "+", "=", "-", "_", "~", "`",
            "{", "}", "[", "]", "|", ":", ";", ",", ".", "/", "?"]
@@ -27,9 +25,9 @@ def generate_password():
     password_entry.insert(0, password)
     pyperclip.copy(password) # to copy entire password to our clipboard just we have to c+v that's it
 
-#TODO -------------------------------------SAVE PASSWORD -------------------------------------------------------------------------------------- #
+#TODO ---------------------------- SAVE PASSWORD --------------------------------------------------------------- #
 def save():
-    domain = website_entry.get()
+    domain = domain_entry.get()
     email = email_entry.get()
     password = password_entry.get() # it will open a pop up UI
 
@@ -44,10 +42,10 @@ def save():
                 domain_entry.delete(0, END)
                 password_entry.delete(0, END)
             # now open UI and provide our credentials it will be described in our file.
-            # to use delete key there are 2 types. first it start of the range 0 and
-            # last we can use it as a key like END but it's optional end of the range.
+            # to use delete key there are 2 types. first it start of the range and
+            # last it use END key optional end of the range.
 
-#TODO ------------------------------------------- UI SETUP ------------------------------------------------------------------------------------ #
+#TODO ---------------------------- UI SETUP ---------------------------------------------------------------------------- #
 
 data = Tk()
 data.title("PASSWORD MANAGER")
@@ -72,7 +70,7 @@ domain_entry.grid(row=1, column=1, columnspan=2)
 domain_entry.focus() # focus the cursor into entry box
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "jaikumarvijay1998@gmail.com") # if we run our UI it will already inserted by my email
+email_entry.insert(0, "anymail@gmail.com") # if we run our UI it will already inserted by my email
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1, columnspan=1)
 
